75843efa37d3a67daf0eef15ba411776ee20df95
dzxrly2 <dzxrly2@gmail.com>
2023-03-09 20:27:17 +0800

    :bookmark: APP基本完成，1.0.0-alpha版本发布

--- app/src/main/java/com/eggtargaryen/bf2042state/component/About.kt
+++ app/src/main/java/com/eggtargaryen/bf2042state/component/About.kt
@@ -2,18 +2,20 @@ package com.eggtargaryen.bf2042state.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.eggtargaryen.bf2042state.R
 import com.eggtargaryen.bf2042state.utils.getYear
 
@@ -21,6 +23,9 @@ import com.eggtargaryen.bf2042state.utils.getYear
 fun About(
     onClose: () -> Unit
 ) {
+    val verticalScrollString = rememberScrollState()
+    val localUriHandler = LocalUriHandler.current
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -45,6 +50,45 @@ fun About(
                 ) {
                     Icon(Icons.Rounded.Close, contentDescription = "Close Drawer")
                 }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .verticalScroll(verticalScrollString),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = stringResource(id = R.string.login_label_about),
+                    style = MaterialTheme.typography.h5,
+                    color = MaterialTheme.colors.primary,
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = stringResource(id = R.string.about_content),
+                    style = MaterialTheme.typography.body1,
+                    color = MaterialTheme.colors.secondary,
+                    textAlign = TextAlign.Justify,
+                    letterSpacing = 1.sp,
+                    lineHeight = 30.sp
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            TextButton(onClick = {
+                localUriHandler.openUri("https://github.com/dzxrly/BF2042State")
+            }) {
+                Text(
+                    text = "Github",
+                    style = MaterialTheme.typography.body1,
+                    color = MaterialTheme.colors.primary,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+                )
             }
         }
         // Bottom
@@ -99,7 +143,7 @@ fun About(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = stringResource(id = R.string.about_made_by),
+                    text = stringResource(id = R.string.about_version),
                     style = MaterialTheme.typography.body1,
                     color = MaterialTheme.colors.primary,
                     textAlign = TextAlign.Center
